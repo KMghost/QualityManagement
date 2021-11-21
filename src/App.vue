093e20f6d6e19c93988b0e9c899d27c262f7b70b
Daniel <wuyh512@foxmail.com>
2021-11-21 17:02:40 +0800

    首页页面

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <div id="app">
-    <h1>测试</h1>
-    <h1>测试vscode提交github</h1>
+     <index/>
   </div>
 </template>
 
 <script>
-
+import index from './components/index'
 export default {
+  components: { index },
   name: 'App',
+  component: {
+    index
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
